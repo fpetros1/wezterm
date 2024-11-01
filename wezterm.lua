1206625205a83cddc451a3d6e4da6fa7bc4537e2
Fernando Petros <fernandopetros@gmail.com>
2024-11-01 01:01:38 +0000

    first commit

---
+++ wezterm.lua
@@ -0,0 +1,137 @@
+local wezterm = require 'wezterm'
+
+function OS_config()
+    local os = Get_os()
+
+    if os == "GNU/Linux" then
+        return Linux_config()
+    end
+
+    if os == "Darwin" then
+        return Mac_config()
+    end
+
+    return wezterm.config_builder()
+end
+
+function Get_os()
+    local osname = "Windows"
+    local fh, _ = assert(io.popen("uname -o 2>/dev/null", "r"))
+    if fh then
+        osname = fh:read()
+    end
+
+    return osname
+end
+
+function Linux_config()
+    local config = wezterm.config_builder()
+    local mux = wezterm.mux
+
+    config.window_padding = {
+        left = 20,
+        right = 20,
+        top = 20,
+        bottom = 20
+    }
+
+    config.window_background_opacity = 0.95
+    config.default_cursor_style = 'BlinkingBlock'
+    config.cursor_blink_rate = 500
+    config.window_close_confirmation = 'NeverPrompt'
+    config.enable_tab_bar = false
+    config.font = wezterm.font {
+        family = 'FiraCode Nerd Font Mono',
+        weight = 'Regular'
+    }
+    config.line_height = 1.2
+    config.font_size = 12
+    config.enable_kitty_graphics = false
+    config.warn_about_missing_glyphs = false
+    config.front_end = "WebGpu"
+    config.term = "xterm-256color"
+    config.color_scheme = 'Eldritch'
+    config.enable_wayland = false
+    config.max_fps = 165
+    config.animation_fps = 165
+    config.default_prog = { "launch-or-attach-tmux" }
+
+    local function is_maximize_desktop()
+        local excluded_desktops = {
+            "COSMIC",
+            "Hyprland",
+        }
+
+        local current_desktop   = os.getenv("XDG_CURRENT_DESKTOP")
+
+        for i = 1, #excluded_desktops do
+            if current_desktop == excluded_desktops[i] then
+                return false
+            end
+        end
+
+        return true
+    end
+
+    wezterm.on('gui-attached', function()
+        if is_maximize_desktop() then
+            local workspace = mux.get_active_workspace()
+            for _, window in ipairs(mux.all_windows()) do
+                if window:get_workspace() == workspace then
+                    window:gui_window():maximize()
+                end
+            end
+        end
+    end)
+
+    return config
+end
+
+function Mac_config()
+    local config = wezterm.config_builder()
+    local mux = wezterm.mux
+
+    config.window_padding = {
+        left = 20,
+        right = 20,
+        top = 20,
+        bottom = 20
+    }
+
+    config.window_background_opacity = 0.95
+    config.default_cursor_style = 'BlinkingBlock'
+    config.cursor_blink_rate = 500
+    config.window_close_confirmation = 'NeverPrompt'
+    config.enable_tab_bar = false
+    config.font = wezterm.font {
+        family = 'FiraCode Nerd Font Mono',
+        weight = 'Regular'
+    }
+    config.line_height = 1.2
+    config.font_size = 15
+    config.enable_kitty_graphics = false
+    config.warn_about_missing_glyphs = false
+    config.front_end = "WebGpu"
+    config.term = "xterm-256color"
+    config.color_scheme = 'Eldritch'
+    config.max_fps = 165
+    config.animation_fps = 165
+    config.default_prog = { "launch-or-attach-tmux" }
+
+    config.set_environment_variables = {
+        PATH = '/usr/local/bin/:' .. os.getenv('PATH')
+    }
+
+    wezterm.on('gui-attached', function()
+        local workspace = mux.get_active_workspace()
+        for _, window in ipairs(mux.all_windows()) do
+            if window:get_workspace() == workspace then
+                window:gui_window():maximize()
+            end
+        end
+    end)
+
+    return config
+end
+
+return OS_config()
